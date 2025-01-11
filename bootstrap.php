7fff3f9cfc5016a75ee9f586472de0af2ac49604
Fishbite <43952420+Fishbite@users.noreply.github.com>
2025-01-11 08:24:46 +0000

    comment added to change config key

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,10 @@ $container->bind('Core\Database', function(){
 
     $config = require base_path('config.php');
 
+    #######################################################
+    ##### Update Array key to 'livedb' for production #####
     return new Database($config['database']);
+    #######################################################
 });
 
 // $db = $container->resolve('Core\Database');
