41a35632e9aee9249985d7b3214116362ceed46d
Fishbite <43952420+Fishbite@users.noreply.github.com>
2025-01-26 08:30:17 +0000

    composer installed

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@
 use Core\Session;
 use Core\ValidationException;
 
-session_start();
-
 /* Document root / project root:
 
     // document root is the `public` folder which has to be
@@ -15,26 +13,31 @@ session_start();
 */
 const BASE_PATH = __DIR__ . '/../';
 
-// echo 'Arse<br>';
 // var_dump(BASE_PATH); // "C:\xampp\htdocs\phpdemo30\public/../"
+# replace the php autoloader with composers' autoloader:
+require BASE_PATH . "/vendor/autoload.php";
+
+session_start();
 
 require BASE_PATH . 'Core/functions.php';
 
-// php calls this function automatically when a class is required
-spl_autoload_register(function ($class) {
+// // php calls this function automatically when a class is required
+// spl_autoload_register(function ($class) {
 
-    // Core\Database - need to replace `\` with `/`
+//     // Core\Database - need to replace `\` with `/`
 
-    // Note: need to escape the searched for `\` with a `\`
-    // use the DIRECTORY_SEPARATOR constant as the `replace`parameter so that it works
-    // across multipple OS's
+//     // Note: need to escape the searched for `\` with a `\`
+//     // use the DIRECTORY_SEPARATOR constant as the `replace`parameter so that it works
+//     // across multipple OS's
 
-    $class = str_replace(search: '\\', replace: DIRECTORY_SEPARATOR, subject: $class);
+//     $class = str_replace(search: '\\', replace: DIRECTORY_SEPARATOR, subject: $class);
 
-    // dd($class);
+//     // dd($class);
 
-    require base_path("{$class}.php");
-});
+//     require base_path("{$class}.php");
+// });
+
+
 
 // appends BASE_PATH to the given path
 // require base_path('Core/router.php');
